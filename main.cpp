c1ae2f7177687fd54bf4890304230239567d7f87
Rodrigo Castro <snowitxc@gmail.com>
2022-04-07 11:49:29 -0300

    add obtenerJugadores function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
-#include "./classes/DtFechaHora/DtFechaHora.cpp"
+#include "./classes/DtFechaHora/DtFechaHora.cpp";
+#include "./classes/DtJugador/dtjugador.cpp";
 
 using namespace std;
+
+
+dtJugador ** obtenerJugadores(int cantJugadores){
+     dtJugador ** jugadoresCopy = new dtJugador*[1000];
+     for(int i = 0 ;  i < cantJugadores && jugadores[i] != NULL; i ++){
+         
+         string nickname = jugadores[i]->getNickname();
+         int edad = jugadores[i]->getEdad();
+         string password = jugadores[i]->getPass();
+
+         dtJugador * jugador = new dtJugador(nickname,edad,password);
+
+         jugadoresCopy[i] = jugador;
+     }
+     return jugadoresCopy;
+}
 
 
 int main(){
