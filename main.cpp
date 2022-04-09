10e1fd7071b670670ba416c338a5aff6c5470100
Maximiliano Olivera <68874048+maxioliverasilva13@users.noreply.github.com>
2022-04-09 15:55:25 -0300

    add void agregarVideoJuegos() (#9)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
 #include<iostream>
-#include "./classes/Sistema/sistema.cpp"
 #include "./classes/DtFechaHora/DtFechaHora.cpp"
-#include "./classes/DtPartidaMultijugador/dtpartidamultijugador.cpp"
-#include "./classes/DtPartidaIndividual/dtpartidaindividual.cpp"
-#include "./classes/DtPartida/dtpartida.cpp"
+#include "./classes/Sistema/sistema.cpp"
 
 using namespace std;
 
@@ -11,11 +8,6 @@ using namespace std;
 int main(){
     int a = 0;
     Sistema * sist = new Sistema();
-    
-    DtFechaHora * fecha = new DtFechaHora(1,1,2022,1,1);
-    DtPartidaMultijugador * test = new DtPartidaMultijugador(true,fecha,5);
-    DtPartidaIndividual * test2 = new DtPartidaIndividual(true,fecha,6);
-
 
     cout << "jajaja";
     return 0;
