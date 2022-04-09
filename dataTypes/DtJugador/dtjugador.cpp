86c3a2093050865a538efe667dee68ce847d4a32
ManuelDiu <mandiu1@hotmail.com>
2022-04-09 18:17:55 -0300

    add class Jugador, update & fix DtJugador
    -implement Jugador class.
    -remove setters from dtJugador.cpp
    -remove password from dtJugador.h

--- dataTypes/DtJugador/dtjugador.cpp
+++ dataTypes/DtJugador/dtjugador.cpp
@@ -2,10 +2,9 @@
 #include "dtjugador.h"
 using namespace std;
 
-dtJugador::dtJugador(string nick, int anios, string passwd){
+dtJugador::dtJugador(string nick, int anios){
     this->nickname = nick;
     this->edad = anios;
-    this->password = passwd;
 }
 
 string dtJugador::getNickname(){
@@ -14,20 +13,4 @@ string dtJugador::getNickname(){
 
 int dtJugador::getEdad(){
     return this->edad;
-}
-
-string dtJugador::getPass(){
-    return this->password;
-}
-
-void dtJugador::setNickname(string a){
-    this->nickname = a;
-}
-
-void dtJugador::setEdad(int b){
-    this->edad = b;
-}
-
-void dtJugador::setPassword(string c){
-    this->password = c;
 }
--- dataTypes/DtJugador/dtjugador.h
+++ dataTypes/DtJugador/dtjugador.h
@@ -8,15 +8,10 @@ class dtJugador{
 private:
     string nickname;
     int edad;
-    string password;
 public:
     dtJugador(string, int, string);
     string getNickname();
     int getEdad();
-    string getPass();
-    void setNickname(string);
-    void setEdad(int);
-    void setPassword(string);
 };
 
 #endif
