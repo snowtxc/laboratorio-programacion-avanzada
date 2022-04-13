2ec7350a19d0489235f431e2da5e77ae9d1f928b
Leonardo Sellanes <pedritodiestro@gmail.com>
2022-04-13 16:59:22 -0300

    implementacion sobrecarga operador

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include<iostream>
-#include "./menu.cpp"
+//#include "./menu.cpp"
 #include <ctime>
+#include "./dataTypes/Sistema/sistema.cpp"
+//#include "./dataTypes/DtPartidaIndividual/dtpartidaindividual.cpp"
 
 using namespace std;
 
 
 int main(){
-    menu();
+    //menu();
+    //Sistema * P1 = new Sistema();
+    time_t t = time(0);
+    tm* now = localtime(&t);
+
+    int dia = now->tm_mday;
+    int mes = 1 + now->tm_mon;
+    int anio = 1900 + now->tm_year;
+    int hora = now->tm_hour;
+    int minuto = now->tm_min;
+    DtFechaHora * ahora = new DtFechaHora(dia, mes, anio, hora, minuto);
+
+    DtPartidaMultijugador P(true, ahora, 25.0);
+    cout << P;
+
     return 0;
 };
